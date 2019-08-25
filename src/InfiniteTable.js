3b37029a66c0f287af74102fa5bb3a738d23e5e4
Mark Jacob <mark.jacob@sap.com>
2019-08-25 22:14:18 +0100

    Profile Picture && City added to table
    - Profile picture added to table using the cellRenderer property on the appropriate column
    - City field added to table

--- src/InfiniteTable.js
+++ src/InfiniteTable.js
@@ -1,5 +1,11 @@
-import React, {useState} from "react";
-import { Table, Column, AutoSizer, List, InfiniteLoader } from "react-virtualized";
+import React, { useState } from "react";
+import {
+  Table,
+  Column,
+  AutoSizer,
+  List,
+  InfiniteLoader
+} from "react-virtualized";
 import "react-virtualized/styles.css";
 
 function InfiniteTable({
@@ -8,19 +14,35 @@ function InfiniteTable({
   /** Callback function responsible for loading the next page of items */
   loadNextPage
 }) {
-
-
+  // Keep track of pageNumber in our state (using the State Hook)
   const [pageNumber, setPageNumber] = useState(1);
 
   const loadMoreRows = () => {
-    let newPageNumber = pageNumber + 1
-    loadNextPage(newPageNumber)
-    setPageNumber(newPageNumber)
-  }
+    let newPageNumber = pageNumber + 1;
+    loadNextPage(newPageNumber);
+    setPageNumber(newPageNumber);
+  };
+
+  // Returns status message when no rows are rendered
+  const onNoRowsRendered = () => {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    );
+  };
+
+  // Takes the image source data from column data and returns an image element
+  const getImageColumn = ({ cellData, dataKey }) => {
+    return (
+      <div>
+        <img src={cellData.thumbnail} />
+      </div>
+    );
+  };
 
   return (
     <div>
-
       <h3>Page count: {pageNumber}</h3>
       <InfiniteLoader
         isRowLoaded={({ index }) => !!list[index]}
@@ -31,6 +53,7 @@ function InfiniteTable({
           <AutoSizer>
             {({ height, width }) => (
               <Table
+                noRowsRenderer={onNoRowsRendered}
                 ref={registerChild}
                 onRowsRendered={onRowsRendered}
                 rowClassName="table-row"
@@ -41,9 +64,37 @@ function InfiniteTable({
                 rowCount={list.length}
                 rowGetter={({ index }) => list[index]}
               >
-                <Column label="First Name" dataKey="first" width={250} cellDataGetter={({dataKey, rowData})=>rowData.name[dataKey]} />
-                <Column label="Last Name" dataKey="last" width={250} cellDataGetter={({dataKey, rowData})=>rowData.name[dataKey]} />
+                <Column
+                  label=""
+                  dataKey="picture"
+                  width={100}
+                  cellRenderer={getImageColumn}
+                />
+                <Column
+                  label="First Name"
+                  dataKey="first"
+                  width={150}
+                  cellDataGetter={({ dataKey, rowData }) =>
+                    rowData.name[dataKey]
+                  }
+                />
+                <Column
+                  label="Last Name"
+                  dataKey="last"
+                  width={150}
+                  cellDataGetter={({ dataKey, rowData }) =>
+                    rowData.name[dataKey]
+                  }
+                />
                 <Column label="E.mail" dataKey="email" width={300} />
+                <Column
+                  label="City"
+                  dataKey="city"
+                  width={150}
+                  cellDataGetter={({ dataKey, rowData }) =>
+                    rowData.location[dataKey]
+                  }
+                />
               </Table>
             )}
           </AutoSizer>
