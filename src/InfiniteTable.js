067cee3827084fa4c3e60df345f219095927eb67
Mark Jacob <mark.jacob@sap.com>
2019-08-27 08:59:44 +0100

    SASS added to project && table styling added

--- src/InfiniteTable.js
+++ src/InfiniteTable.js
@@ -8,6 +8,7 @@ import {
 } from "react-virtualized";
 import _ from "lodash";
 import "react-virtualized/styles.css";
+import "./InfiniteTable.scss";
 
 function InfiniteTable({
   /** List of items loaded so far */
@@ -18,8 +19,8 @@ function InfiniteTable({
   // Keep track of pageNumber, sortBy and sortDirection in our state (using the State Hook)
   // TODO: re-evaluate this, may be cleaner to convert to Class and use setState() depending on state updates
   const [pageNumber, setPageNumber] = useState(1);
-  const [sortBy, setSortBy] = useState('dataKey');
-  const [sortDirection, setSortDirection] = useState('ASC');
+  const [sortBy, setSortBy] = useState("dataKey");
+  const [sortDirection, setSortDirection] = useState("ASC");
 
   // Executes callback method responsible for requesting more user data
   const loadMoreRows = () => {
@@ -37,6 +38,12 @@ function InfiniteTable({
     );
   };
 
+  // Handler for click event on table row
+  const onRowClick = ({ event, index, rowData }) => {
+    console.log(`ON ROW CLICK ` + index);
+    console.log(rowData);
+  };
+
   // Takes the image source data from column data and returns an image element
   const getImageColumn = ({ cellData, dataKey }) => {
     return (
@@ -47,15 +54,15 @@ function InfiniteTable({
   };
 
   /**
-   * 
+   *
    */
   const sortColumn = ({ sortBy, sortDirection }) => {
-    setSortBy(sortBy)
-    setSortDirection(sortDirection)
+    setSortBy(sortBy);
+    setSortDirection(sortDirection);
 
     list.sort(function(a, b) {
       // Using lodash's get() to allow using sortBy which can be a nested object property string like ('name.last') or ('location.city')
-      var strA = _.get(a, sortBy); 
+      var strA = _.get(a, sortBy);
       var strB = _.get(b, sortBy);
 
       // Compare strings and take into account the sortDirection
@@ -65,75 +72,84 @@ function InfiniteTable({
       if (strA > strB) {
         return sortDirection === "ASC" ? -1 : 1;
       }
-    
+
       // strings must be equal
       return 0;
-    })
-  }
-
+    });
+  };
 
   return (
     <div>
-      <h3>Page count: {pageNumber}</h3>
+      <h2>Infinite Scrolling Users</h2>
+      <p>
+        This table is displaying <b>{list.length} users</b> from a{" "}
+        <a href="https://randomuser.me/">random user generator API</a>.</p> 
+        
+      <p>The
+        table is "infinite scrolling", so, as you scroll down the table more
+        user requests will be created and more user details will be displayed.
+      </p>
       <InfiniteLoader
         isRowLoaded={({ index }) => !!list[index]}
         loadMoreRows={loadMoreRows}
         rowCount={1000000}
       >
         {({ onRowsRendered, registerChild }) => (
-          <AutoSizer>
-            {({ height, width }) => (
-              <Table
-                noRowsRenderer={onNoRowsRendered}
-                ref={registerChild}
-                onRowsRendered={onRowsRendered}
-                rowClassName="table-row"
-                headerHeight={40}
-                width={width}
-                height={500}
-                rowHeight={40}
-                rowCount={list.length}
-                rowGetter={({ index }) => list[index]}
-                sort={sortColumn}
-                sortBy={sortBy}
-                sortDirection={sortDirection}
-              >
-                <Column
-                  label=""
-                  dataKey="picture"
-                  width={100}
-                  disableSort
-                  cellRenderer={getImageColumn}
-                />
-                <Column
-                  label="First Name"
-                  dataKey="name.first"
-                  width={150}
-                  defaultSortDirection="ASC"
-                  cellDataGetter={({ dataKey, rowData }) =>
-                    rowData.name.first
-                  }
-                />
-                <Column
-                  label="Last Name"
-                  dataKey="name.last"
-                  width={150}
-                  cellDataGetter={({ dataKey, rowData }) =>
-                    rowData.name.last
-                  }
-                />
-                <Column label="E.mail" dataKey="email" width={300} />
-                <Column
-                  label="City"
-                  dataKey="location.city"
-                  width={150}
-                  cellDataGetter={({ dataKey, rowData }) =>
-                    rowData.location.city
-                  }
-                />
-              </Table>
-            )}
-          </AutoSizer>
+          <div style={{ flex: "1 1 auto", height: "85vh" }}>
+            <AutoSizer>
+              {({ height, width }) => (
+                <Table
+                  className="usersTable bordered striped"
+                  noRowsRenderer={onNoRowsRendered}
+                  ref={registerChild}
+                  onRowsRendered={onRowsRendered}
+                  onRowClick={onRowClick}
+                  rowClassName="table-row"
+                  headerHeight={40}
+                  width={width}
+                  height={height}
+                  rowHeight={40}
+                  rowCount={list.length}
+                  rowGetter={({ index }) => list[index]}
+                  sort={sortColumn}
+                  sortBy={sortBy}
+                  sortDirection={sortDirection}
+                >
+                  <Column
+                    label=""
+                    dataKey="picture"
+                    width={100}
+                    disableSort
+                    cellRenderer={getImageColumn}
+                  />
+                  <Column
+                    label="First Name"
+                    dataKey="name.first"
+                    width={150}
+                    defaultSortDirection="ASC"
+                    cellDataGetter={({ dataKey, rowData }) =>
+                      rowData.name.first
+                    }
+                  />
+                  <Column
+                    label="Last Name"
+                    dataKey="name.last"
+                    width={150}
+                    cellDataGetter={({ dataKey, rowData }) => rowData.name.last}
+                  />
+                  <Column label="E.mail" dataKey="email" width={300} />
+                  <Column
+                    label="City"
+                    dataKey="location.city"
+                    width={150}
+                    cellDataGetter={({ dataKey, rowData }) =>
+                      rowData.location.city
+                    }
+                  />
+                </Table>
+              )}
+            </AutoSizer>
+          </div>
         )}
       </InfiniteLoader>
     </div>
